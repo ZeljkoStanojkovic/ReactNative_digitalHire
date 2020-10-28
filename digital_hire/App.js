891d5d7a2b6131e4a87b0313bda40690cb456ddc
ZeljkoStanojkovic <zeljko1991work@gmail.com>
2020-10-28 17:35:52 +0800

    first commit

---
+++ digital_hire/App.js
@@ -0,0 +1,19 @@
+import React, { PureComponent } from "react";
+import Router from "./src/router";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  ScrollView,
+  Image,
+} from 'react-native'
+
+export default class Driver extends PureComponent {
+  render() {
+    return (
+       <Router />
+    );
+  }
+}
